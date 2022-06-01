d6696bc65a8c5c7521eb0a0ebf4ac1e9094fd7e7
kamalMohamed <kmohamed@turndigital.net>
2022-06-01 12:37:34 +0200

    dev:front - add Modal Card

---
+++ src/components/InfoCard.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <!-- Modal -->
+    <div
+      class="modal fade"
+      id="appModal"
+      tabindex="-1"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">{{ title }}</h5>
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div class="modal-body">
+            <slot></slot>
+          </div>
+          <div class="modal-footer">
+            <button
+              type="button"
+              class="btn btn-secondary"
+              data-bs-dismiss="modal"
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "InfoCard",
+  props: ["title"],
+};
+</script>
